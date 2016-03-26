8597ddb75d90884ada6118f55bbf1d534fa18f84
rahulrainahome <rahulrainahome@live.com>
2016-03-26 15:32:59 +0530

    Dashboard added

---
+++ app/src/main/java/client/app/clientapp/DashboardActivity.java
@@ -0,0 +1,16 @@
+package client.app.clientapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+
+    }
+}
