a1f0a37990f3c9251b06ccb02513f837ac1ae657
rahulrainahome <rahulrainahome@live.com>
2016-04-02 18:42:33 +0530

    changes done.

--- app/src/main/java/client/app/clientapp/broadcast/SmsBroadcastReceiver.java
+++ app/src/main/java/client/app/clientapp/broadcast/SmsBroadcastReceiver.java
@@ -4,16 +4,27 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.text.TextUtils;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import client.app.clientapp.utils.Constants;
 
 public class SmsBroadcastReceiver extends BroadcastReceiver {
+
+    Context ctxt;
     public SmsBroadcastReceiver() {
+
     }
 
     @Override
@@ -21,6 +32,7 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
 
         Toast.makeText(context, "Client App SMS Received", Toast.LENGTH_LONG).show();
 
+        ctxt = context;
         Bundle intentExtras = intent.getExtras();
         if (intentExtras != null) {
             Object[] sms = (Object[]) intentExtras.get("pdus");
@@ -40,4 +52,63 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
             }
         }
     }
+
+    public class registerTask extends AsyncTask<String, String, String>
+    {
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                URL url = new URL(Constants.REGISTER_DEVICE + "?CATEGORY=default");
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                int statusCode = urlConnection.getResponseCode();
+                if (statusCode ==  200)
+                {
+                    InputStream it = new BufferedInputStream(urlConnection.getInputStream());
+                    InputStreamReader read = new InputStreamReader(it);
+                    BufferedReader buff = new BufferedReader(read);
+                    StringBuilder dta = new StringBuilder();
+                    String chunks ;
+                    while((chunks = buff.readLine()) != null)
+                    {
+                        dta.append(chunks);
+                    }
+                    return dta.toString();
+                }
+                else
+                {
+                    return "ERROR";
+                }
+
+            }
+            catch (Exception e)
+            {
+                return "EXCEPTION";
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            if(s.contains("EXCEPTION") || s.contains("ERROR"))
+            {
+
+            }
+            else
+            {
+
+            }
+            Toast.makeText(ctxt, "" + s, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/client/app/clientapp/service/SMSCategoryRegService.java
@@ -0,0 +1,21 @@
+package client.app.clientapp.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class SMSCategoryRegService extends Service {
+    public SMSCategoryRegService() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+
+
+        return null;
+    }
+
+}
