2c13aa757f31b759f5ed338cfe6502d49b0a2e6a
rahulrainahome <rahulrainahome@live.com>
2016-04-11 02:46:10 +0530

    changes done.

--- app/src/main/java/client/app/clientapp/activity/NotificationActivity.java
+++ app/src/main/java/client/app/clientapp/activity/NotificationActivity.java
@@ -4,20 +4,28 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import client.app.clientapp.R;
+import client.app.clientapp.adapter.NotificationAdapter;
 
 public class NotificationActivity extends AppCompatActivity {
 
 
+    ListView lw;
+    NotificationAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
         SharedPreferences s = getSharedPreferences("info_push", 0);
-        String data = s.getString("data","****");
+        String data = s.getString("data","[]");
+
+        lw = (ListView)findViewById(R.id.listView);
+        adapter = new NotificationAdapter(this, 4);
+        lw.setAdapter(adapter);
 
 
 
--- app/src/main/java/client/app/clientapp/adapter/NotificationAdapter.java
+++ app/src/main/java/client/app/clientapp/adapter/NotificationAdapter.java
@@ -1,7 +1,35 @@
 package client.app.clientapp.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import java.util.zip.Inflater;
 
-public class NotificationAdapter  {
+import client.app.clientapp.R;
+
+
+public class NotificationAdapter extends ArrayAdapter<Integer> {
+
+    Activity activity;
+    LayoutInflater inflater;
+
+    public NotificationAdapter(Activity activity, int count) {
+
+        super(activity, R.layout.list_item_notification, count);
+
+        this.activity = activity;
+        this.inflater = activity.getLayoutInflater();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = inflater.inflate(R.layout.list_item_notification, null);
+
+        return view;
+    }
 }
