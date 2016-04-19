f076bf62e0196cc6d791777cd25153abc0f308c9
rahulrainahome <rahulrainahome@live.com>
2016-04-19 16:56:55 +0530

    changes in address page.

---
+++ app/src/main/java/client/app/clientapp/activity/AddressActivity.java
@@ -0,0 +1,15 @@
+package client.app.clientapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import client.app.clientapp.R;
+
+public class AddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+}
